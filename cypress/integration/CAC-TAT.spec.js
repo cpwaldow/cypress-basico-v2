408ca1b4f68819ebc6dbedb5fdebe68878560806
Carlos Eduardo <cpwaldow@gmail.com>
2022-12-28 10:34:30 -0300

    verifica titulo da aplicacao

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,5 +1,10 @@
-describe('teste', () => {
-  it('teste 1', () => {
-    cy.visit('./src/');
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html');
+  });
+  it('01 - verifica o título da aplicação', () => {
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
   });
 });
